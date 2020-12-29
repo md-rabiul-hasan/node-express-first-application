14537fe9be0ad3b18cbf0ac89af4a7de3302d333
md-rabiul-hasan <rabiul.fci@gmail.com>
2020-12-29 23:48:36 +0600

    post request added with body-parser package

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require('body-parser');
+const { request, response } = require("express");
+
+app.use(bodyParser.json());
+
 app.get('/', (request, response) => {
     response.end("Hello Arif");
 });
+
+app.post('/post', (request, response) => {
+    let newResponse = {
+        "time" : Date.now(),
+        "response_code" : 200,
+        "message" : `Hello ${request.body.name}, How are you ?`
+    }
+    response.json(newResponse);
+} );
 
 app.listen(3000);
