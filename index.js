907e9f9714980b1ac43d8131462607fae148c690
md-rabiul-hasan <rabiul.fci@gmail.com>
2020-12-30 00:00:01 +0600

    project small picies with module

--- index.js
+++ index.js
@@ -2,21 +2,10 @@ const express = require("express");
 const app = express();
 
 const bodyParser = require('body-parser');
-const { request, response } = require("express");
-
 app.use(bodyParser.json());
 
-app.get('/', (request, response) => {
-    response.end("Hello Arif");
-});
+const route = require('./route');
+route(app);
 
-app.post('/post', (request, response) => {
-    let newResponse = {
-        "time" : Date.now(),
-        "response_code" : 200,
-        "message" : `Hello ${request.body.name}, How are you ?`
-    }
-    response.json(newResponse);
-} );
 
 app.listen(3000);
---
+++ route.js
@@ -0,0 +1,14 @@
+module.exports = function(app){
+    app.get('/', (request, response) => {
+        response.end("Hello Arif");
+    });
+    
+    app.post('/post', (request, response) => {
+        let newResponse = {
+            "time" : Date.now(),
+            "response_code" : 200,
+            "message" : `Hello ${request.body.name}, How are you ?`
+        }
+        response.json(newResponse);
+    } );    
+}
